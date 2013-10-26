4c79b0472a10a1dd7792d1fa418216feac66e666
Giordano Scalzo <giordano.scalzo@gmail.com>
2013-10-26 13:33:16 +0100

    Creating the Evil Invaders from Space

--- ReactiveInvaders/ECViewController.m
+++ ReactiveInvaders/ECViewController.m
@@ -7,7 +7,7 @@
 //
 
 #import "ECViewController.h"
-#import "ECMyScene.h"
+#import "GameScene.h"
 
 @implementation ECViewController
 
@@ -21,7 +21,7 @@
     skView.showsNodeCount = YES;
     
     // Create and configure the scene.
-    SKScene * scene = [ECMyScene sceneWithSize:skView.bounds.size];
+    SKScene * scene = [GameScene sceneWithSize:skView.bounds.size];
     scene.scaleMode = SKSceneScaleModeAspectFill;
     
     // Present the scene.
